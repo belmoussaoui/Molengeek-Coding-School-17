fc41cb0983322975845b9845ccd465aeaae81d7f
belmoussaoui <emb-12@outlook.com>
2021-02-24 16:49:47 +0100

    Ajout Jour 49 - 24/02

--- js/12-dom/4-events/2-cs-dom/public/js/exo1.js
+++ js/12-dom/4-events/2-cs-dom/public/js/exo1.js
@@ -1,4 +1,4 @@
-// 1 exo counter
+// 1
 const button = document.querySelector("button");
 button.addEventListener("click", () => {
     document.querySelector("span").textContent++;
@@ -8,11 +8,11 @@ button.addEventListener("click", () => {
 const square = document.getElementById("square");
 square.addEventListener("click", () => {
     square.classList.toggle("on");
+    square.classList.toggle("off");
     square.textContent = square.classList.contains("on") ? "on" : "off";
 });
 
 // 3
-
 let numberSwitch = true;
 document.addEventListener("keydown", (event) => {
     if (event.key === "6") {
@@ -35,3 +35,30 @@ function removeNumberTitles() {
         h2.textContent = `${text.slice(text.indexOf(" ") + 1)}`;
     }
 }
+
+// 4
+const multi = document.getElementById("multi-click");
+let state = 0;
+multi.addEventListener("click", () => {
+    state = (state + 1) % 3;
+    console.log(state);
+    switch (state) {
+        case 0:
+            multi.style.borderRadius = "0";
+            multi.style.backgroundColor = "white";
+            break;
+        case 1:
+            multi.style.borderRadius = "100%";
+            break;
+        case 2:
+            multi.style.backgroundColor = "red";
+            break;
+    }
+});
+
+// 5
+const color = document.getElementById("pick-color");
+color.addEventListener("change", () => {
+    const p = document.querySelector("#pick-color + p");
+    p.style.color = color.value;
+});
---
+++ js/12-dom/4-events/2-cs-dom/public/js/exo2.js
@@ -0,0 +1,102 @@
+// 1
+const button1 = document.querySelector("button");
+button1.addEventListener("click", () => {
+    const img = document.querySelector("#mignon-container img");
+    const container = document.getElementById("mignon-container");
+    container.appendChild(img.cloneNode());
+});
+
+// 2
+let container = document.getElementById("mignon2-container");
+let imgs = container.children;
+
+container.addEventListener("click", (e) => {
+    if (e.target.tagName === "IMG") {
+        e.target.remove();
+        exoBonus(e);
+    }
+});
+
+function exoBonus(e) {
+    if (imgs.length === 0) {
+        setTimeout(() => {
+            for (let i = 0; i < 5; i++) {
+                container.appendChild(e.target.cloneNode());
+            }
+        }, 1000);
+    }
+}
+
+// const container = document.querySelector("#mignon2-container");
+// removable(container);
+
+// function removable(container) {
+//     const mignons = container.querySelectorAll("*");
+//     for (const img of mignons) {
+//         img.addEventListener("click", () => {
+//             let node = container.removeChild(img);
+//             restart(node);
+//         });
+//     }
+// }
+
+// function restart(node) {
+//     if (container.childElementCount === 0) {
+//         setTimeout(() => {
+//             for (let i = 0; i < 5; i++) {
+//                 container.append(node.cloneNode());
+//             }
+//             removable(container);
+//         }, 1000);
+//     }
+// }
+
+// 3
+let li = document.createElement("li");
+li.textContent = "li num 2";
+let refChild = document.querySelectorAll("ul li")[1];
+let ul = document.querySelector("ul");
+ul.insertBefore(li, refChild);
+
+// 4
+let submit = document.querySelector("#exo-li-from-input button");
+let ulTask = document.querySelector("#exo-li-from-input ul");
+let input = document.querySelector("#exo-li-from-input input");
+
+submit.addEventListener("click", () => {
+    let li = document.createElement("li");
+    li.textContent = input.value;
+    input.value = "";
+    // https://stackoverflow.com/a/43838105
+    ulTask.prepend(li);
+});
+
+//bonus
+
+let index1 = -1;
+let index2 = -1;
+let onActive = false;
+let edit = document.querySelectorAll("#exo-li-from-input button")[1];
+edit.addEventListener("click", () => {
+    onActive = !onActive;
+    edit.style.backgroundColor = onActive ? "green" : "#f8f9fa";
+
+    let allli = document.querySelectorAll("#exo-li-from-input li");
+    for (let li of allli) {
+        li.addEventListener("click", () => {
+            if (onActive) {
+                if (index1 === -1) {
+                    allli = document.querySelectorAll("#exo-li-from-input li");
+                    index1 = [...allli].indexOf(li);
+                } else {
+                    allli = document.querySelectorAll("#exo-li-from-input li");
+                    index2 = [...allli].indexOf(li);
+                    ulTask.insertBefore(allli[index1], allli[index2 + 1]);
+                    ulTask.insertBefore(allli[index2], allli[index1 + 1]);
+                    index1 = -1;
+                    index2 = -1;
+                }
+            }
+        });
+    }
+});
